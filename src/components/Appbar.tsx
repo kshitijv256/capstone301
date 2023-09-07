432bedaf15e52c0b45f6b8b825910d3b3505f520
Kshitij Verma <kshitijv250@gmail.com>
2023-09-07 16:28:15 +0530

    Improved appbar UI

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -7,7 +7,7 @@ import { Link, useLocation } from "react-router-dom";
 import { ThemeContext } from "../context/theme";
 import SettingModal from "../views/settings";
 
-const userNavigation = [{ name: "Sign out", href: "/logout" }];
+const userNavigation = [{ name: "Log out", href: "/logout" }];
 
 const classNames = (...classes: string[]): string =>
   classes.filter(Boolean).join(" ");
@@ -105,6 +105,14 @@ const Appbar = () => {
                         </Link>
                       ))}
                     </div>
+                    {auth && (
+                      <button
+                        onClick={() => setIsOpen(true)}
+                        className="text-gray-900 dark:text-slate-300"
+                      >
+                        Settings
+                      </button>
+                    )}
                     <div className="flex gap-2">
                       <span className="text-gray-900 dark:text-slate-300">
                         DarkMode{" "}
@@ -200,15 +208,17 @@ const Appbar = () => {
                         leaveFrom="transform opacity-100 scale-100"
                         leaveTo="transform opacity-0 scale-95"
                       >
-                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white dark:bg-slate-700 text-gray-800 dark:text-gray-300 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                           {userNavigation.map((item) => (
                             <Menu.Item key={item.name}>
                               {({ active }) => (
                                 <a
                                   href={item.href}
                                   className={classNames(
-                                    active ? "bg-gray-100" : "",
-                                    "block px-4 py-2 text-sm text-gray-700"
+                                    active
+                                      ? "bg-slate-200/50 dark:bg-slate-600/50"
+                                      : "",
+                                    "w-full text-left block px-4 py-2 text-sm"
                                   )}
                                 >
                                   {item.name}
@@ -221,8 +231,10 @@ const Appbar = () => {
                               <button
                                 onClick={() => setIsOpen(true)}
                                 className={classNames(
-                                  active ? "bg-gray-100" : "",
-                                  "block px-4 py-2 text-sm text-gray-700"
+                                  active
+                                    ? "bg-slate-200/50 dark:bg-slate-600/50"
+                                    : "",
+                                  "w-full text-left block px-4 py-2 text-sm"
                                 )}
                               >
                                 Settings
@@ -271,65 +283,6 @@ const Appbar = () => {
                 pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white dark:bg-slate-300 shadow-lg ring-0 transition duration-200 ease-in-out`}
                   />
                 </Switch> */}
-                <div className="ml-3">
-                  {auth ? (
-                    <Menu as="div" className="relative">
-                      <div>
-                        <Menu.Button
-                          className="rounded-full bg-white p-1 text-gray-400 hover:text-lime-600 
-                                    dark:bg-slate-600 dark:text-slate-300 dark:hover:text-lime-500"
-                        >
-                          <UserCircleIcon
-                            className="h-6 w-6"
-                            aria-hidden="true"
-                          />
-                        </Menu.Button>
-                      </div>
-                      <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                      >
-                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          {userNavigation.map((item) => (
-                            <Menu.Item key={item.name}>
-                              {({ active }) => (
-                                <a
-                                  href={item.href}
-                                  className={classNames(
-                                    active ? "bg-gray-100" : "",
-                                    "block px-4 py-2 text-sm text-gray-700"
-                                  )}
-                                >
-                                  {item.name}
-                                </a>
-                              )}
-                            </Menu.Item>
-                          ))}
-                          <Menu.Item>
-                            {({ active }) => (
-                              <button
-                                onClick={() => setIsOpen(true)}
-                                className={classNames(
-                                  active ? "bg-gray-100" : "",
-                                  "block px-4 py-2 text-sm text-gray-700"
-                                )}
-                              >
-                                Settings
-                              </button>
-                            )}
-                          </Menu.Item>
-                        </Menu.Items>
-                      </Transition>
-                    </Menu>
-                  ) : (
-                    <div></div>
-                  )}
-                </div>
               </div>
             </div>
             <SettingModal open={isOpen} setOpen={setIsOpen} />
